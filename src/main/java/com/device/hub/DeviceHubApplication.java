388f23c3069e226df05d597876d2b967e13e9dcc
Dilnei Rodrigues da Cunha <dilneicunha@gmail.com>
2024-11-18 22:35:02 +0000

    feat: First commit

---
+++ src/main/java/com/device/hub/DeviceHubApplication.java
@@ -0,0 +1,13 @@
+package com.device.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceHubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeviceHubApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/device/hub/DeviceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.device.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceHubApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
